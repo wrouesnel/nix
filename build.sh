c327684a53344c8452836bbca2220668bb36d7e4
Will Rouesnel <wrouesnel@wrouesnel.com>
2025-06-06 14:10:30 +1000

    Build helpers

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# See: https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+# Note: you can't refactor this out: its at the top of every script so the scripts can find their includes.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+CFLAGS="-DDEBUG -ggdb -O0" CXXFLAGS="-DDEBUG -ggdb -O0" \
+  ./configure $configureFlags --prefix=$(pwd) --enable-gc --disable-shared
